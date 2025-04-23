9be521be5bda9e6d704be490ae257e53c9f89783
Kate Shepel <MariyaKaterina@gmail.com>
2025-04-23 13:59:57 +0300

    fix colors and sizes

--- src/components/organisms/CartItem/CartItem.tsx
+++ src/components/organisms/CartItem/CartItem.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './CartItem.scss';
-import CloseLogo from '../../../../public/icons/close-icon.svg?react';
+import CloseIcon from '../../../../public/icons/close-icon.svg?react';
+import MinusIcon from '../../../../public/icons/minus-icon.svg?react';
+import PlusIcon from '../../../../public/icons/plus-icon.svg?react';
 
 const tempCartItem = {
   "id": 1,
@@ -23,33 +25,20 @@ export const CartItem: React.FC = () => {
       <div className='cart-item'>
         <div className='cart-item__body'>
           <button className='cart-item__close-btn'>
-            <CloseLogo />
-            {/* <img
-              className='cart-item__close-btn--svg'
-              src='../../../public/icons/close-icon.svg'
-              alt='close button'>
-            </img> */}
+            <CloseIcon className='cart-item__close-btn--svg' />
           </button>
           <img className='cart-item__image' src={tempCartItem.image} alt={tempCartItem.name}></img>
-          <h3 className='cart-item__name'>{tempCartItem.name}</h3>
+          <span className='cart-item__name'>{tempCartItem.name}</span>
         </div>
 
         <div className='cart-item__footer'>
           <div className='counter'>
             <button className='counter__decrease'>
-              <img
-                className='counter__decrease--svg'
-                src='../../../public/icons/minus-icon.svg'
-                alt='decrease button'>
-              </img>
+              <MinusIcon className='counter__decrease--svg' />
             </button>
             <p className='counter__amount'> 1 </p>
             <button className='counter__increase'>
-              <img
-                className='counter__decrease--svg'
-                src='../../../public/icons/plus-icon.svg'
-                alt='increase button'>
-              </img>
+              <PlusIcon className='counter__increase--svg' />
             </button>
           </div>
 
