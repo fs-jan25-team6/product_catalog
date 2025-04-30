04369d2029ca09f30d8f8f419f64fb0190b5ee40
Mariia Kapustkina <kapustkina08@mail.com>
2025-04-30 20:34:13 +0300

    PCT6-53 created breadcrumbs

--- src/components/pages/Layout.tsx
+++ src/components/pages/Layout.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../Header';
 import { Footer } from '../Footer/Footer';
+import { Breadcrumbs } from '../Breadcrumbs/Breadcrumbs';
 
 export const AppLayout: React.FC = () => {
   return (
     <div className="content">
       <Header />
+      <Breadcrumbs />
       <main className="main">
         <section className="container">
           <Outlet />
